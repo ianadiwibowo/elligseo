614ad4b7ca7a828ec9c4b984f6933a03a1d1f4ad
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-22 23:40:44 +0700

    Try some basic arithmetics and string interpolation

--- hello_world.exs
+++ hello_world.exs
@@ -1,1 +1,38 @@
-IO.puts "Hello world from Elixir"
+# Hello World
+
+IO.puts("Hello world from Elixir")
+
+# Basic
+
+12 / 10
+# => 1.2 -- Float division
+div(12, 10)
+# => 1 -- Integer division
+round(2.5)
+# => 3
+is_boolean(false)
+# => true
+is_boolean(5)
+# => false
+is_integer(5)
+# => true
+is_float(5.5)
+# => true
+is_number(0x1A)
+# => true
+is_number(7.0e-10)
+# => true
+trunc(5.78)
+# => 5 -- Get the integer part of a float
+
+# Ruby-like symbol (atom)
+:cat == :cat
+# => true
+is_atom(:bird)
+# => true
+
+# String interpolation
+
+name = "Lupita"
+IO.puts "Hello #{name}"
+# => "Hello Lupita"
