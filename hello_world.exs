54a1184e9e6826076aec2e092092de4015c4ea7c
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-24 01:42:38 +0700

    Assign and reassign by pattern matching

--- hello_world.exs
+++ hello_world.exs
@@ -26,9 +26,9 @@ is_number(7.0e-10)
 # => true
 trunc(5.78)
 # => 5 -- Get the integer part of a float
-i "hello"
-i 'hello'
-i 5
+i("hello")
+i('hello')
+i(5)
 # -- Identifying variable types
 
 ## Ruby-like symbol (atom)
@@ -83,7 +83,7 @@ length([1, 2, true, 3])
 [1, 2, 3, 4] -- [2, 3]
 # => [1,4] -- List subtraction
 
-list = [1,2,3,4]
+list = [1, 2, 3, 4]
 hd(list)
 # 1 -- Head
 tl(list)
@@ -99,3 +99,31 @@ tuple_size(t)
 # => 3
 put_elem(t, 1, :bupita)
 # => {:lupita, :bupita, :kupita} -- Overwrite element at index 1
+
+## Pattern matching
+
+{a, b, c} = {:pupuru, "neko", 88}
+# a => :pupuru -- Can be used for mass assignment, with tuples matching
+# b => "neko"
+# c => 88
+
+{:ok, result} = {:ok, 200}
+# result => 200 -- Only match if the right tuple starts with :ok
+{:ok, result} = {:error, 401}
+# => ** (MatchError) no match of right hand side value: {:error, 401}
+
+## Pin operator
+# Use when we want to pattern match against a variable's existing value
+# rather than rebinding (reassigning) the variable
+
+[^x, 2, 3] = [1, 2, 3]
+# => [1, 2, 3]
+{y, ^x} = {2, 1}
+# => {2, 1}
+{y, ^x} = {2, 2}
+# => ** (MatchError) no match of right hand side value: {2, 2}
+{y, 1} = {2, 2}
+# => ** (MatchError) no match of right hand side value: {2, 2}
+# Similar like this, because x was already bound to value 1
+[a, _] = [5, 6]
+# -- Ommit the second variable, disposable
